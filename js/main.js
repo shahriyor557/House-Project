d2d33d5d41ec79dff32197bf7290806f546df5e5
sohibxon <muhammadiyevsohibnazar@gmail.com>
2024-05-20 10:50:17 +0500

    g

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let menyu = document.querySelector(".menu");
+let madol = document.querySelector(".madol");
+let svg = document.querySelector("svg");
+menyu.addEventListener("click", function () {
+    madol.classList.toggle("active");
+})
+svg.addEventListener("click", function () {
+    madol.classList.toggle("active");
+})
